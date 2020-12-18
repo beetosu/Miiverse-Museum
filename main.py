614ae50572f140635fba58f52eae66279bfedf98
beetosu <cpwalsh@buffalo.edu>
2020-12-18 00:50:20 -0500

    fix timing issues

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import database, compose, drawing, secrets
 import sqlite3, time, tweepy
+from datetime import datetime, timedelta
 from PIL import Image
 
 #this is what is called, and where the main tweet loop is
@@ -14,12 +15,19 @@ def main():
     auth.set_access_token(secrets.ACCESS_TOKEN, secrets.ACCESS_SECRET)
     api = tweepy.API(auth)
 
+    #wait for the first interval (12a, 4a, 8a, etc. in EST)
+    currentHour = datetime.now().replace(second=0, microsecond=0, minute=0) - timedelta(hours=5)
+    currentHour += timedelta(hours=1)
+    while currentHour.hour % 4 != 0:
+        currentHour += timedelta(hours=1)
+    currentHour += timedelta(hours=5)
+    time.sleep((currentHour - datetime.now()).total_seconds())
+
     #for every post, get the needed data, format it into a tweet,
     #update the database to reflect the posting, and then wait 4 hours
     while len(posts) > 0:
         start = time.time()
         post = posts.pop(0)
-        print(post)
         body = compose.create_tweet_text(post)
         postedDrawing = drawing.get_drawing(post[2])
         postedDrawing.save("drawing.png")
@@ -27,7 +35,8 @@ def main():
         cur.execute("UPDATE Posts SET Posted=1 WHERE Id='"+post[0]+"'")
         conn.commit()
         end = time.time()
-        time.sleep(14400 - (start - end))
+        durationTime = end - start
+        time.sleep(14400 - durationTime)
     conn.close()
 
 if __name__ == "__main__":
